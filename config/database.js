5329c5ba5e46fc3f09b2a4be9d5419579fb5abea
MicheleDore <michele.dore3@gmail.com>
2022-09-26 10:38:20 +0000

    init

---
+++ config/database.js
@@ -0,0 +1,11 @@
+import mysql from 'mysql'
+
+let pool  = mysql.createPool({
+  connectionLimit : 10000,
+    host: "db.3wa.io",
+    user: "micheledore",
+    password: "a46ab1857dab77efa5438c79652e287c",
+    database: "micheledore_blog",
+});
+
+export default pool
